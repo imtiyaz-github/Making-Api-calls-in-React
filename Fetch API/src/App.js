3bc6ab2a936b0e818b1c117387787f61f75f609e
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-05-24 23:31:15 +0530

    changes made

--- Fetch API/src/App.js
+++ Fetch API/src/App.js
@@ -1,41 +1,60 @@
 import React, { useState } from "react";
 import MovieList from "./components/MovieList";
-// import Movie from "./components/Movie";
 import "./App.css";
 
-function App() {
-  const [movies, setMovies] = useState([]);
 
-  async function fetchMoviesHandler() {
-    try {
-      const respose = await fetch("https://swapi.dev/api/films/");
-      const data = await respose.json();
+// function App() {
+//   const [movies, setMovies] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
 
-      const transFormMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-          directorName: movieData.producer,
-        };
-      });
-      setMovies(transFormMovies);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+//   async function fetchMoviesHandler() {
+//     setIsLoading(true);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MovieList movies={movies} />
-      </section>
-    </React.Fragment>
-  );
+//     const respose = await fetch("https://swapi.dev/api/films/");
+//     const data = await respose.json();
+
+//     const transFormMovies = data.results.map((movieData) => {
+//       return {
+//         id: movieData.episode_id,
+//         title: movieData.title,
+//         openingText: movieData.opening_crawl,
+//         releaseDate: movieData.release_date,
+//         directorName: movieData.producer,
+//       };
+//     });
+//     setIsLoading(false);
+//     setMovies(transFormMovies);
+//     // setIsLoading(false);
+//   }
+
+//   return (
+//     <React.Fragment>
+//       <section>
+//         <button onClick={fetchMoviesHandler} className="button">
+//           Fetch Movies
+//         </button>
+//       </section>
+//       <section>
+//         {!isLoading && movies.length < 0 && <MovieList movies={movies} />}
+//         {!isLoading && <p>Found no movies </p>}
+//         {isLoading && <p>Loading...</p>}
+//       </section>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
+const App =() =>{
+ 
+   const[movies,setMovies] = useState([]);
+
+   const fetchMoviesHandler= () =>{
+    
+   }
+
+
+
+
 }
-
 export default App;
